accbe3e81c7ab2f8c2b237b1e6774178e90b1a81
SumDumIdiut <oliverprakke@gmail.com>
2025-06-30 13:24:57 +0200

    Fix

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// Place this file manually on your web host (not in GitHub if you want to keep keys out of the repo)
+window.firebaseConfig = {
+  apiKey: "AIzaSyB_IaCbvWDqeQor2vvvtwYSywaxSj-6YvI",
+  authDomain: "pokerweb-5d5ea.firebaseapp.com",
+  projectId: "pokerweb-5d5ea",
+  storageBucket: "pokerweb-5d5ea.firebasestorage.app",
+  messagingSenderId: "563976234474",
+  appId: "1:563976234474:web:b16ec64d73c69eb9ffbe93",
+  measurementId: "G-FZMTKB7CZ6",
+  databaseURL: "https://pokerweb-5d5ea-default-rtdb.europe-west1.firebasedatabase.app"
+};
